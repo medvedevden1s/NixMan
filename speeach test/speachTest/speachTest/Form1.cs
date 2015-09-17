81323a60dbe7a7cab1902e45ae35d813be84fd3c
Medvedev Denis <dmedvedv@connecture.com>
2015-09-17 18:43:12 +0300

    add Synthesis

--- speeach test/speachTest/speachTest/Form1.cs
+++ speeach test/speachTest/speachTest/Form1.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Speech.Recognition;
-
+using System.Speech.Synthesis;
 
 namespace speachTest
 {
     public partial class Form1 : Form
     {
         SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
-        
+        SpeechSynthesizer syncSpeechSynthesizer = new SpeechSynthesizer();
 
         public Form1()
         {
@@ -19,6 +19,7 @@ namespace speachTest
         {
             recEngine.RecognizeAsync(RecognizeMode.Multiple);
             btnDisable.Enabled = true;
+            btnEnable.Enabled = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -41,17 +42,28 @@ namespace speachTest
             switch (e.Result.Text)
             {
                 case "say hello": 
-                    MessageBox.Show("Hello Denis. How are you?"); break;
-                
+                    //MessageBox.Show("Hello Denis. How are you?"); break;
+
+                    PromptBuilder promtBuilder = new PromptBuilder();
+                    promtBuilder.StartSentence();
+                    promtBuilder.AppendText("Hello Denis");
+                    promtBuilder.EndSentence();
+
+                    promtBuilder.AppendBreak(PromptBreak.ExtraSmall);
+                    promtBuilder.AppendText("How are you?");
+
+                    syncSpeechSynthesizer.SpeakAsync("Hello Denis. How are you?"); break;
                 case "print my name":
                     richTextBox1.Text += "\nDenis"; break;
-
+                case "speak selected text":
+                    syncSpeechSynthesizer.SpeakAsync(richTextBox1.SelectedText); break;
             }
         }
 
         private void btnDisable_Click(object sender, EventArgs e)
         {
             recEngine.RecognizeAsyncStop();
+            btnEnable.Enabled = true;
             btnDisable.Enabled = false;
         }
     }
